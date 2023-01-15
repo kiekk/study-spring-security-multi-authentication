29eb99f0448250cece2e6f9fdf804812f2065b15
soonho <shyoon991@gmail.com>
2023-01-16 01:35:10 +0900

    Initial Commit

---
+++ src/main/java/com/example/multiauthentication/StudySpringSecurityMultiAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.multiauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringSecurityMultiAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringSecurityMultiAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/multiauthentication/StudySpringSecurityMultiAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multiauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringSecurityMultiAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
