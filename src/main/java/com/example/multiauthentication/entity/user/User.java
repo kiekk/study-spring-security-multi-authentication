ffd300430a778ff8e5d718a1a13535506112ff49
soonho <shyoon991@gmail.com>
2023-04-09 01:35:10 +0900

    add entity User

---
+++ src/main/java/com/example/multiauthentication/entity/user/User.java
@@ -0,0 +1,40 @@
+package com.example.multiauthentication.entity.user;
+
+public class User {
+    private String id;
+    private String name;
+    private String loginId;
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
